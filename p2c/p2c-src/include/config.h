e9e422e48590b11bf115ba27b3b2ed82720bca34
gaius <gaius>
2002-04-17 14:27:51 +0000

    Initial revision

---
+++ p2c/p2c-src/include/config.h
@@ -0,0 +1,13 @@
+#include "../auto-host.h"
+#include "ansidecl.h"
+#include "system.h"
+
+
+#ifdef FILE       /* a #define in BSD, a typedef in SYSV (hp-ux, at least) */
+# ifndef BSD
+#  define BSD 1
+# endif
+#endif
+
+
+
