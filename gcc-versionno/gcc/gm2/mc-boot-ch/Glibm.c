9b3d0f5d87f74ab1701cc9280913fc527cfed7d0
gaius <gaius@gnu.org>
2017-01-15 21:54:48 +0000

    * added gm2/m2-boot-ch/Glibm.c * added gm2/m2-boot-ch/Gnetwork.h * added gm2/m2-boot-ch/network.c

---
+++ gcc-versionno/gcc/gm2/mc-boot-ch/Glibm.c
@@ -0,0 +1,111 @@
+/* automatically created by mc.  */
+
+#define _libm_C
+#include <math.h>
+#include "Glibm.h"
+
+
+double libm_pow (double x, double y)
+{
+  return pow (x, y);
+}
+
+float libm_powf (float x, float y)
+{
+  return powf (x, y);
+}
+
+long double libm_powl (long double x, long double y)
+{
+  return powl (x, y);
+}
+
+double libm_sqrt (double x)
+{
+  return sqrt (x);
+}
+
+float libm_sqrtf (float x)
+{
+  return sqrtf (x);
+}
+
+long double libm_sqrtl (long double x)
+{
+  return sqrtl (x);
+}
+
+double libm_asin (double x)
+{
+  return asin (x);
+}
+
+float libm_asinf (float x)
+{
+  return asinf (x);
+}
+
+long double libm_asinl (long double x)
+{
+  return asinl (x);
+}
+
+double libm_atan (double x)
+{
+  return atan (x);
+}
+
+float libm_atanf (float x)
+{
+  return atanf (x);
+}
+
+long double libm_atanl (long double x)
+{
+  return atanl (x);
+}
+
+double libm_atan2 (double x, double y)
+{
+  return atan2 (x, y);
+}
+
+float libm_atan2f (float x, float y)
+{
+  return atan2f (x, y);
+}
+
+long double libm_atan2l (long double x, long double y)
+{
+  return atan2l (x, y);
+}
+
+double libm_sin (double x)
+{
+  return sin (x);
+}
+
+float libm_sinf (float x)
+{
+  return sinf (x);
+}
+
+long double libm_sinl (long double x)
+{
+  return sinl (x);
+}
+
+double libm_cos (double x)
+{
+  return cos (x);
+}
+
+float libm_cosf (float x)
+{
+  return cosf (x);
+}
+
+long double libm_cosl (long double x)
+{
+  return cosl (x);
+}
