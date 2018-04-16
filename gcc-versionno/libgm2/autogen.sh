2d2b02dc87494b5e685aec7678998b2681920143
gaius <gaius@gnu.org>
2018-04-16 20:12:21 +0100

    * gm2/Make-lang.in:  changed the plugin directory to   plugin rather than gm2/plugin.  Corrected .so typeos. * gm2/gm2.texi:  Semantic checking (New section). * libgm2/Makefile.am:  specify the libtool location. * libgm2/Makefile.in:  regenerated. * libgm2/aclocal.m4:  regenerated. * libgm2/autogen.sh:  corrected script to regenerate   using the correct macros and versions of autoconf tools. * libgm2/configure:  regenerated. * libgm2/configure.ac:  regenerated. * libgm2/libcor/Makefile.in:  regenerated. * libgm2/libiso/Makefile.in:  regenerated. * libgm2/liblog/Makefile.in:  regenerated. * libgm2/libmin/Makefile.in:  regenerated. * libgm2/libpim/Makefile.in:  regenerated. * libgm2/libulm/Makefile.in:  regenerated.

--- gcc-versionno/libgm2/autogen.sh
+++ gcc-versionno/libgm2/autogen.sh
@@ -4,11 +4,8 @@ rm -rf autom4te.cache
 
 # libtoolize
 rm -f aclocal.m4
-aclocal -I config -I ../config
-automake --add-missing
-# automake
-autoconf
-# autoreconf2.64
+aclocal-1.11 -I . -I config -I ../config
+autoreconf2.64
 
 rm -rf autom4te.cache
 
