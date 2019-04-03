4fb400d3fa2b6f9265e55ed697ab3fefade57faf
gaius <gaius@gnu.org>
2019-04-03 11:24:09 +0100

    * libgm2/Makefile.in:  regenerated. * libgm2/aclocal.m4:  regenerated. * libgm2/autogen.sh:  removed version numbers. * libgm2/config/libtool.m4:  new version of the file   taken from libgo/config. * libgm2/configure:  regenerated. * libgm2/configure.ac:  updated version numbers. * libgm2/libcor/Makefile.in:  regenerated. * libgm2/libiso/Makefile.in:  regenerated. * libgm2/liblog/Makefile.in:  regenerated. * libgm2/libmin/Makefile.in:  regenerated. * libgm2/libpim/Makefile.in:  regenerated. * libgm2/libulm/Makefile.in:  regenerated.

--- gcc-versionno/libgm2/autogen.sh
+++ gcc-versionno/libgm2/autogen.sh
@@ -4,8 +4,8 @@ rm -rf autom4te.cache
 
 # libtoolize
 rm -f aclocal.m4
-aclocal-1.11 -I . -I config -I ../config
-autoreconf2.64
+aclocal -I . -I config -I ../config
+autoreconf
 
 rm -rf autom4te.cache
 
