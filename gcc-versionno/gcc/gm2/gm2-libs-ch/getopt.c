e7b5f0d371fa6521569b63bd78eb4cdb90788fa8
Gaius Mulley <gaius@debian>
2019-07-28 00:50:20 +0100

    * gm2/gm2-libs-ch/getopt.c:  changed licence to LGPL 3. * gm2/gm2-libs/GetOpt.def:  changed licence to LGPL 3. * gm2/gm2-libs/GetOpt.mod:  changed licence to LGPL 3. * gm2/gm2-libs/getopt.def:  changed licence to LGPL 3.

--- gcc-versionno/gcc/gm2/gm2-libs-ch/getopt.c
+++ gcc-versionno/gcc/gm2/gm2-libs-ch/getopt.c
@@ -1,23 +1,22 @@
 /* getopt.c provide access to the C getopt library.
 
 Copyright (C) 2017-2019 Free Software Foundation, Inc.
-Contributed by Gaius Mulley <gaius@glam.ac.uk>.
+Contributed by Gaius Mulley <gaius.mulley@southwales.ac.uk>.
 
 This file is part of GNU Modula-2.
 
-GNU Modula-2 is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
+GNU Modula-2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
 GNU Modula-2 is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GNU Modula-2; see the file COPYING.  If not,
-see <https://www.gnu.org/licenses/>.  */
+You should have received a copy of the GNU Lesser General Public License
+along with GNU Modula-2.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include <unistd.h>
 #include <stdlib.h>
