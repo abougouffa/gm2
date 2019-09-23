baac5f4478a79643b944c66ea5fd4c97bd146aa5
gaius <gaius@gnu.org>
2019-09-23 13:30:38 +0100

    * gm2/Make-lang.in:  removed reference to sckt.c. * gm2/configure.in:  updated dates. * gm2/gm2-lang.h:  updated dates. * gm2/gm2-tree.h:  updated dates. * gm2/gpl.texi:  combined dates. * gm2/m2pp.h:  fixed boilerplate header. * gm2/m2pp.c:  updated dates.

--- gcc-versionno/gcc/gm2/m2pp.h
+++ gcc-versionno/gcc/gm2/m2pp.h
@@ -1,13 +1,8 @@
-/* Copyright (C) 2007, 2008, 2009, 2010
- *               Free Software Foundation, Inc.
- *
- *  Gaius Mulley <gaius@glam.ac.uk> constructed this file.
- *  It was built by borrowing code from the gcc-.../gcc/c-*.c files
- *  and its function is to aid debugging of the GNU Modula-2 front
- *  end.  It prints the GCC trees in Modula-2 format.
- */
+/* m2pp.h pretty print trees, output in Modula-2 where possible.
 
-/*
+Copyright (C) 2007-2019 Free Software Foundation, Inc.
+Contributed by Gaius Mulley <gaius@glam.ac.uk>.
+
 This file is part of GNU Modula-2.
 
 GNU Modula-2 is free software; you can redistribute it and/or modify
@@ -15,16 +10,15 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3, or (at your option)
 any later version.
 
-GNU Modula-2 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Modula-2 is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU Modula-2; see the file COPYING.  If not, write to the
 Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301, USA.
-*/
+02110-1301, USA.  */
 
 #if !defined(M2PP_H)
 #   define M2PP_H
